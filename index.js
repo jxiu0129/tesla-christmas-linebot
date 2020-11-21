35c9ef21bc63da8378dd98c1892ec1c587110ece
Brian Hsieh <h.s.i.e.h.tw.29@gmail.com>
2020-11-21 23:40:27 +0800

    暫時完成：新增名字map，然後寫了一個array判斷當下使用者，以防機器人炸開

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Client, middleware } from '@line/bot-sdk';
 import express from 'express';
 import { imageMap, carousel } from './template';
+import { questions, mapReceiver } from './text';
 
 const lineConfig = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || 'ohQ35vtFDgdq/n7r++Eays1NQYBGOqvxN6msZtq6IA0FhlJfq0xFk/+EmjXNvEUGbyrVQgoha8u5nYRU10+As4Gkg9F7B1gp0QM+CFxawIDCNpOoo4/w1QCp5lXpHI+NFXyZ1DbqhWzJzztcRm9iIQdB04t89/1O/w1cDnyilFU=',
@@ -11,6 +12,8 @@ const client = new Client(lineConfig);
 const app = express();
 
 let userId, userName, userPictureUrl; //getProfile用
+
+let userArray = [];
 
 app.post('/', middleware(lineConfig), async (req, res) => {
     // Promise
@@ -40,24 +43,10 @@ app.post('/', middleware(lineConfig), async (req, res) => {
             ],
             "destination": "Uc50e84eafc37e51091664f6759c51cc2"
         }*/
-        client.getProfile(req.body.events[0].source.userId).then((profile) => {
+        await client.getProfile(req.body.events[0].source.userId).then((profile) => {
             userId = profile.userId;
             userName = profile.displayName;
             userPictureUrl = profile.pictureUrl;
-            /*{ 實驗getProfile
-                userId: 'uuid here',
-                displayName: '杰修',
-                pictureUrl: 'https://profile.line-scdn.net/xxx',
-                statusMessage: '(robber)(dumpling)',
-                language: 'en' 
-            }*/
-        });
-
-        // 實驗getNumberOfMessageDeliveries(date: string):
-        client.getNumberOfFollowers('20200622').then( (res)=> {
-            console.log(`getNumberOfFollowers: ${JSON.stringify(res)}`);
-        }).catch((err) => {
-            console.log(err);
         });
 
         let result = await req.body.events.map(handleEvent);
@@ -73,10 +62,14 @@ const handleEvent = (event) => {
     case 'join': //加入別人的群組
     case 'follow': //追蹤
         // console.log(JSON.stringify(event));
+        // return client.replyMessage(event.replyToken, {
+        // type: 'text',
+        // text: `我知道你是誰喔，你的id是${event.source.userId}\n\n你在${Date(event.timestamp)}加我為好友`
+        // });   
         return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: `我知道你是誰喔，你的id是${event.source.userId}\n\n你在${Date(event.timestamp)}加我為好友`
-        });   
+            type: 'text',
+            text: `Hello ${userName}!\n感謝您一年來的付出\n您已受邀參加本次的Tesla聖誕饗宴\n會由我來幫您尋找您的交換禮物送禮對象!!\n\n請輸入ok來開始回答問題！`
+            });
     case 'message': //傳訊息給機器人
         switch (event.message.type) {
         case 'text':
@@ -89,25 +82,60 @@ const handleEvent = (event) => {
     }
 }
 
+
+
+// let qCounter = 0;
+// let receiver;
 const textHandler = (replyToken, inputText) => {
     try{
         let resText;
-        switch (inputText) {
-            case '你好':
-                resText = '你好啊';
-                break;
-            case 'test':
-                resText = `測試`;
-                break
-            case 'Q&A':
-                return client.replyMessage(replyToken, imageMap());
-            case 'q&a':
-                return client.replyMessage(replyToken, carousel());
-            case '我是誰':
-                resText = `你的id為: ${userId}\n你的名字為: ${userName}\n你的照片為: ${userPictureUrl}`;
-                break;
-            default:
-                resText = '請親臨院所';
+
+        let userCheck = userArray.find(user => user.name === userName);
+        console.log('userArray ===> ', userArray);
+        console.log('userCheck ===> ', userCheck);
+
+        if (!userCheck) {
+            userArray.push({
+                name: userName,
+                qCounter: 0,
+                receiver: null
+            });
+            console.log('second userArray ===> ',userArray)
+        }
+
+        userCheck = userArray.find(user => user.name === userName);
+        console.log('second userCheck ===> ', userCheck);
+
+        if(inputText === 'rs') {
+            userCheck.qCounter = 0;
+            console.log('重新');
+        }
+        
+        if(inputText) {
+            if(userCheck.qCounter === 1){
+                if(inputText !== 'Danny'){
+                    userCheck.receiver = mapReceiver[inputText];
+                    if(userCheck.receiver === undefined) {
+                        return client.replyMessage(replyToken, {
+                            type: 'text',
+                            text: `奇怪，不太對喔～ 請檢查你名字字首有沒有大寫，或是把自己名字拼錯呢？`
+                        });
+                    }
+                } else {
+                    // 判斷Danny
+                    return client.replyMessage(replyToken, {
+                        type: 'text',
+                        text: '請問你是 李展源 還是 李治 呢？'
+                    });
+                }
+            }
+            if(userCheck.qCounter < 5) {
+                resText = questions[userCheck.qCounter];
+                userCheck.qCounter++;
+            } else {
+                userCheck.qCounter = 0;
+                resText = `漂亮!!${userName}果然是特斯拉的傑出員工!!\n您這次的送禮對象為 ${userCheck.receiver}\n請精心為他挑選禮品並附上手寫小卡片一張\uDBC0\uDC84`;
+            }
         }
         return client.replyMessage(replyToken, {
             type: 'text',
---
+++ text.js
@@ -0,0 +1,43 @@
+export const questions = [
+    '首先，請問一下您的英文名是?',
+    '再來，簡單幾個小問題~\n請問您在這工作多久了?',
+    '有毅力! 真是辛苦您的付出了...\n\n您目前工作的據點是?',
+    '真好! 這裡合作氣氛很好~\n\n交換一個小秘密...\n您最喜歡怎麼樣個性的同事呢?\n溫柔、熱情、積極、善良、還是風趣呢?',
+    '原來如此..我了解了\n\n最後，請您輸入通關密語!\nTesla 的 Misson 是甚麼!!\n答對後即可成功找到本次您的送禮對象',
+];
+
+export const mapReceiver = {
+    'Stacy': 'Emma',
+    'Yvonne': 'Eddie',
+    'Josh': '李治',
+    'Tom' : 'Patrick',
+    'Peggy': 'Andrew',
+    'Vincent': 'Joe',
+    'William': 'Anne',
+    'Mandy': '李展源',
+    'Andy': 'Grant',
+    'Andrew': 'Aston',
+    'Hydi': 'Josh',
+    'Anne': 'William',
+    'Emma': 'Yvonne',
+    'Edward': 'Stacy',
+    'Sherry': 'Morris',
+    'Grant': 'Tom',
+    'Joe': 'Andy',
+    'Carol': 'Rex',
+    'Aston': 'Sherry',
+    'Eddie': 'Mandy',
+    'Jeffery': 'Peggy',
+    'Morris': 'Vincent',
+    'Patrick': 'Jeffery',
+    'Jonason': 'Hydi',
+    'Rex': 'Edward',
+    '李展源': 'Carol',
+    '李治': 'Jonason',
+};
+
+
+
+// Danny 
+
+
